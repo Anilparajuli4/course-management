2ff3a0e3eba5800498ab98a106bb280b18920d83
Anil Parajuli <anilparajuli998@gmail.com>
2025-01-26 20:39:56 +0545

    changed base url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://course-management-9uxk.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
